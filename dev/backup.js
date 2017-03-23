0ad1bf9a49ea401d1c6ea9f36d298d938eca204d
jamesbenedict <jabenedi@indiana.edu>
2017-03-22 20:48:26 -0400

    got responsive graph working

--- dev/backup.js
+++ dev/backup.js
@@ -1,4 +1,95 @@
- 
+var Chart = (function(window,d3) {
+
+var svg, data, x, y, xAxis, yAxis, dim, chartWrapper, line, path, margin = {}, width, height;
+var breakPoint = 768;
+
+  d3.csv('js/data.csv', init); //load data, then initialize chart
+
+  //called once the data is loaded
+  function init(csv) {
+    data = csv;
+
+    //initialize scales
+    xExtent = d3.extent(data, function(d,i) { return new Date(d.date) });
+    yExtent = d3.extent(data, function(d,i) { return d.value });
+    x = d3.time.scale().domain(xExtent);
+    y = d3.scale.linear().domain(yExtent);
+
+    //initialize axis
+    xAxis = d3.svg.axis().orient('bottom');
+    yAxis = d3.svg.axis().orient('left');
+
+    //the path generator for the line chart
+    line = d3.svg.line()
+      .x(function(d) { return x(new Date(d.date)) })
+      .y(function(d) { return y(d.value) });
+
+    //initialize svg
+    svg = d3.select('#chart').append('svg');
+    chartWrapper = svg.append('g');
+    path = chartWrapper.append('path').datum(data).classed('line', true);
+    chartWrapper.append('g').classed('x axis', true);
+    chartWrapper.append('g').classed('y axis', true);
+
+    //render the chart
+    render();
+  }
+
+  function render() {
+
+    //get dimensions based on window size
+    updateDimensions(window.innerWidth);
+
+    //update x and y scales to new dimensions
+    x.range([0, width]);
+    y.range([height, 0]);
+
+    //update svg elements to new dimensions
+    svg
+      .attr('width', width + margin.right + margin.left)
+      .attr('height', height + margin.top + margin.bottom);
+    chartWrapper.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    //update the axis and line
+    xAxis.scale(x);
+    yAxis.scale(y);
+
+    svg.select('.x.axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis);
+
+    svg.select('.y.axis')
+      .call(yAxis);
+
+    path.attr('d', line);
+
+    yAxis.scale(y).orient(window.innerWidth < breakPoint ? 'right' : 'left');
+
+  }
+
+  function updateDimensions(winWidth) {
+    margin.top = 20;
+    // margin.right = 50;
+    // margin.left = 50;
+    margin.right = winWidth < breakPoint ? 0 : 50;
+    margin.left = winWidth < breakPoint ? 0 : 50;
+    margin.bottom = 50;
+
+    width = winWidth - margin.left - margin.right;
+    // height = 500 - margin.top - margin.bottom;
+    height = .7 * width; //aspect ratio is 0.7
+
+  }
+
+  return {
+    render : render
+  }
+
+})(window,d3);
+
+window.addEventListener('resize', Chart.render);
+
+
 var annotations = [
   {
     "cx": 691,
@@ -35,27 +126,24 @@ var annotations = [
   }
 ]
 var units = "People";
- 
+
+var w = document.getElementById('id');
+
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
     width = 1200 - margin.left - margin.right,
-    height = 740 - margin.top - margin.bottom;
+    height = 640 - margin.top - margin.bottom;
  
 var formatNumber = d3.format(",.0f"),    // zero decimal places
     format = function(d) { return formatNumber(d) + " " + units; };
     // color = d3.scale.category20();
 
  // append the svg canvas to the page
-var svg = d3.select("#chart")
-    .classed("svg-container", true)
-    .append("svg")
-   .attr("preserveAspectRatio", "xMinYMin meet")
-   .attr("viewBox", "0 0 1200 740")
-      .classed("svg-content-responsive", true)
-
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", 
           "translate(" + margin.left + "," + margin.top + ")");
-
 
 
 // Make ring notes draggable (good for testing, probably not useful for final version)
@@ -74,7 +162,7 @@ var sankey = d3.sankey()
 var path = sankey.link();
  
 // load the data
-d3.json("data.json", function(error, graph) {
+d3.json("js/data.json", function(error, graph) {
  
     var nodeMap = {};
     graph.nodes.forEach(function(x) { nodeMap[x.name] = x; });
@@ -176,4 +264,19 @@ d3.json("data.json", function(error, graph) {
 
 
 
+function resize() {
+  var w = document.getElementById("container").offsetWidth;
+  var h = document.getElementById("container").offsetHeight;
  
+  return w, h 
+}
+
+
+resize();
+
+// console.log(w);
+window.addEventListener('resize', function(event){
+  // w = resize()[0];
+  // h = resize()[1];
+  
+});
